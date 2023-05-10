b3a4a8217b0992f22152c90ec4aed73791a10d17
Giovanellucas <129345132+Giovanellucas@users.noreply.github.com>
2023-05-10 20:55:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const express = require("express");
 
 const server = express();
 
-server.get("/health",(req,res)=>{
-    res.send(`server is running`);
+{
+let products = [
+    {
+    id:1,
+    name: "Iphone Apple",
+    description: "Iphone X a venda na olx",
+    isDone: false,
+},
+{
+    id:2,
+    name: "Notebook Acer",
+    description: "Notebook acer usado",
+    isDone: false,
+}
+]
+}
+
+server.get("/products", (req, res)=>{
+    res.json({
+        products: [
+
+        ]
+    })
 })
 
 const port = 3000;
-server.listen(port, ()=> {
-    console.log(`server is running ${port}`);
-});
+server.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
